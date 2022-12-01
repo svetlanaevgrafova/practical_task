2377f3982d33b33de805210701cea43eccd08d3b
minada <minada93@gmail.com>
2022-12-01 14:56:06 +0400

    add pagination functionality for the results list

--- index-applicants.php
+++ index-applicants.php
@@ -16,15 +16,36 @@ if (!$mysqli) {
   die("Connection failed: " . mysqli_connect_error());
 }
 
-// GET
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-  $list_of_applicants = $mysqli->query("SELECT * FROM applicants");
-
-  while($result = mysqli_fetch_array($list_of_applicants, MYSQLI_ASSOC)) {
-    $applicants[] = $result;
-  }
+// Поверка, есть ли GET запрос
+if (isset($_GET['page'])) {
+  // Если да то переменной $page присваиваем его
+  $page = $_GET['page'];
+} else { // Иначе
+  // Присваиваем $page один
+  $page = 1;
+}
+// Назначаем количество данных на одной странице
+$size_page = 5;
+// Вычисляем с какого объекта начать выводить
+$offset = ($page-1) * $size_page;
+// SQL запрос для получения количества элементов
+$count_sql = "SELECT COUNT(*) FROM applicants";
+// Отправляем запрос для получения количества элементов
+$amount_applicants = mysqli_query($mysqli, $count_sql);
+// Получаем результат
+$total_rows = mysqli_fetch_array($amount_applicants)[0];
+// Вычисляем количество страниц
+$total_pages = ceil($total_rows / $size_page);
+// Создаём SQL запрос для получения данных
+$sql = "SELECT * FROM applicants LIMIT $offset, $size_page";
+// Отправляем SQL запрос
+$res_data = mysqli_query($mysqli, $sql);
+// Цикл для вывода строк
+while($row = mysqli_fetch_array($res_data, MYSQLI_ASSOC)){
+  // Выводим логин пользователя
+  $applicants[] = $row;
 }
 require 'page-applicants.html';
 
-exit;
+
