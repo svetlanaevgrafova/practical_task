faeb12d5aa5115a1e09531dc5400a94c4ad1e2c7
minada <minada93@gmail.com>
2022-12-08 14:07:20 +0400

    change of access rights

--- download.php
+++ download.php
@@ -9,24 +9,23 @@ clearstatcache();
 //Проверьте, существует ли путь к файлу или нет
 if(file_exists($url)) {
 
-//Определение информации заголовка
-header('Content-Description: File Transfer');
-header('Content-Type: application/octet-stream');
-header('Content-Disposition: attachment; filename="'.basename($url).'"');
-header('Content-Length: '. filesize($url));
-header('Pragma: public');
+  //Определение информации заголовка
+  header('Content-Description: File Transfer');
+  header('Content-Type: application/octet-stream');
+  header('Content-Disposition: attachment; filename="'.basename($url).'"');
+  header('Content-Length: '. filesize($url));
+  header('Pragma: public');
 
-//Очистить выходной буфер системы
-flush();
+  //Очистить выходной буфер системы
+  flush();
 
-//Считайте размер файла
-readfile($url,true);
+  //Считайте размер файла
+  readfile($url,true);
 
-//Завершить работу со скриптом
-die();
-}
-else{
-echo "Путь к файлу не существует.";
+  //Завершить работу со скриптом
+  die();
+} else {
+  echo "Путь к файлу не существует.";
 }
 }
 echo "Путь к файлу не определен."
